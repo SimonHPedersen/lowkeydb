7d10ce323b7873fb0e26b1c75c569bd2414589a4
Simon Hem Pedersen <shp@trifork.com>
2012-09-18 11:38:40 +0200

    initial backend

---
+++ src/riak_kv_lowkeydb_backend.erl
@@ -0,0 +1,98 @@
+-module(riak_kv_lowkeydb_backend).
+%%-behavior(riak_kv_backend).
+
+%% Riak Storage Backend API
+-export([%%api_version/0,
+         start/2
+         %%stop/1,
+         %%get/3,
+         %%put/5,
+         %%delete/4,
+         %%drop/1,
+         %%fold_buckets/4,
+         %%fold_keys/4,
+         %%fold_objects/4,
+         %%is_empty/1,
+         %%status/1,
+         %%callback/3]
+         ]).
+
+-record(state, {folder_ref :: string()}).
+
+-type state() :: #state{}.
+-type config() :: [].
+
+%% ===================================================================
+%% Public API
+%% ===================================================================
+
+%% @doc Return the major version of the
+%% current API and a capabilities list.
+%% The current valid capabilities are async_fold
+%% and indexes.
+%%-spec api_version() -> {integer(), [atom()]}.
+
+%% @doc Start the backend
+-spec start(integer(), config()) -> {ok, state()} | {error, term()}.
+start(Partition, Config) -> 
+  Folder = app_helper:get_prop_or_env(folder, Config, lowkey),
+  io:fwrite("Folder: ~w ~n", [Folder]),
+
+ {ok, #state{}}.
+
+ %%folder_ref=Folder}}.
+
+
+%% @doc Stop the backend
+%%-spec stop(state()) -> ok.
+
+%% @doc Retrieve an object from the backend
+%%-spec get(riak_object:bucket(), riak_object:key(), state()) ->
+%%                 {ok, any(), state()} |
+%%                 {ok, not_found, state()} |
+%%                 {error, term(), state()}.
+
+
+
+%% @doc Insert an object into the backend.
+%%-type index_spec() :: {add, Index, SecondaryKey} | {remove, Index, SecondaryKey}.
+%%-spec put(riak_object:bucket(), riak_object:key(), [index_spec()], binary(), state()) ->
+%%                 {ok, state()} |
+%%                 {error, term(), state()}.
+
+%% @doc Delete an object from the backend
+%%-spec delete(riak_object:bucket(), riak_object:key(), [index_spec()], state()) ->
+%%                    {ok, state()} |
+%%                    {error, term(), state()}.
+
+%% @doc Fold over all the buckets
+%%-spec fold_buckets(riak_kv_backend:fold_buckets_fun(),
+%%                   any(),
+%%                   [],
+%%                   state()) -> {ok, any()} | {async, fun()}.
+
+%% @doc Fold over all the keys for one or all buckets.
+%%-spec fold_keys(riak_kv_backend:fold_keys_fun(),
+%%                any(),
+%%                [{atom(), term()}],
+%%                state()) -> {ok, term()} | {async, fun()}.
+
+%% @doc Fold over all the objects for one or all buckets.
+%%-spec fold_objects(riak_kv_backend:fold_objects_fun(),
+%%                   any(),
+%%                   [{atom(), term()}],
+%%                   state()) -> {ok, any()} | {async, fun()}.
+
+%% @doc Delete all objects from this backend
+%% and return a fresh reference.
+%%-spec drop(state()) -> {ok, state()} | {error, term(), state()}.
+
+%% @doc Returns true if this backend contains any
+%% non-tombstone values; otherwise returns false.
+%%-spec is_empty(state()) -> boolean() | {error, term()}.
+
+%% @doc Get the status information for this backend
+%%-spec status(state()) -> [{atom(), term()}].
+
+%% @doc Register an asynchronous callback
+%%-spec callback(reference(), any(), state()) -> {ok, state()}.
